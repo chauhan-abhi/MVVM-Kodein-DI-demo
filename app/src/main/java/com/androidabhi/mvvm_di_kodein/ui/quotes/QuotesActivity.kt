39906a142a6de00c483da9856dd9ef5c3c03c767
chauhan-abhi <chauhanabhi1245@gmail.com>
2018-12-14 16:07:35 +0530

    initial commit setting up androidx lifecycle and kodein

---
+++ app/src/main/java/com/androidabhi/mvvm_di_kodein/ui/quotes/QuotesActivity.kt
@@ -0,0 +1,13 @@
+package com.androidabhi.mvvm_di_kodein.ui.quotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.androidabhi.mvvm_di_kodein.R
+
+class QuotesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quotes)
+    }
+}
